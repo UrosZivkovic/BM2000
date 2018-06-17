d0e955dbf6d0138ce22aa74b8bc901b4aadcc4bf
Nemanja <nemanja.m.303@gmail.com>
2018-06-17 05:00:56 +0200

    opet wsp

--- BM/serverApp/server/app.js
+++ BM/serverApp/server/app.js
@@ -13,53 +13,51 @@ const productRoutes = require("../api/routes/products");
 const userRoutes = require("../api/routes/users");
 
 mongoose.connect(
-    "mongodb://BM2000Admin:bmadmin@bloodmasters2000-shard-00-00-xxpcx.mongodb.net:27017,bloodmasters2000-shard-00-01-xxpcx.mongodb.net:27017,bloodmasters2000-shard-00-02-xxpcx.mongodb.net:27017/test?ssl=true&replicaSet=BloodMasters2000-shard-0&authSource=admin"
+  "mongodb://BM2000Admin:bmadmin@bloodmasters2000-shard-00-00-xxpcx.mongodb.net:27017,bloodmasters2000-shard-00-01-xxpcx.mongodb.net:27017,bloodmasters2000-shard-00-02-xxpcx.mongodb.net:27017/test?ssl=true&replicaSet=BloodMasters2000-shard-0&authSource=admin"
 );
-const productRoutes = require('../api/routes/products');
-const userRoutes = require('../api/routes/users');
-const zavodRoutes = require('../api/routes/zavodi');
-const obavestenjeRoutes = require('../api/routes/obavestenja');
-const novostRoutes = require('../api/routes/novosti');
-
+const productRoutes = require("../api/routes/products");
+const userRoutes = require("../api/routes/users");
+const zavodRoutes = require("../api/routes/zavodi");
+const obavestenjeRoutes = require("../api/routes/obavestenja");
+const novostRoutes = require("../api/routes/novosti");
 
 app.use(morgan("dev")); //za logovanje zahteva
-app.use(bodyParser.urlencoded({extended: false})); //samo prosti zahtevi
+app.use(bodyParser.urlencoded({ extended: false })); //samo prosti zahtevi
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*"); //CORS???
-    res.header("Access-Control-Allow-Headers", "Authorization");
-    if (req.method === "OPTIONS") {
-        res.header("Access-Control-Alow-Methods", "PUT, POST, PATCH, DELETE, GET");
-        return res.status(200).json({});
-    }
-    next();
+  res.header("Access-Control-Allow-Origin", "*"); //CORS???
+  res.header("Access-Control-Allow-Headers", "Authorization");
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Alow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
 });
 app.use(cors());
 
-app.use('/products',productRoutes);
-app.use("/users",userRoutes);
-app.use("/obavestenja",obavestenjeRoutes);
-app.use("/novosti",novostRoutes);
-app.use("/zavodi",zavodRoutes);
+app.use("/products", productRoutes);
+app.use("/users", userRoutes);
+app.use("/obavestenja", obavestenjeRoutes);
+app.use("/novosti", novostRoutes);
+app.use("/zavodi", zavodRoutes);
 
-app.use(express.static(path.join(__dirname +'/../dist/')));
-
+app.use(express.static(path.join(__dirname + "/../dist/")));
 
 //app.use('/',(req,res,next)=>{res.status(200).json({message:"it works!"})});
 
 app.use((req, res, next) => {
-    const error = new Error("Page not found");
-    error.status = 404;
-    next(error);
+  const error = new Error("Page not found");
+  error.status = 404;
+  next(error);
 });
 app.use((error, req, res, next) => {
-    res.status(error.status || 500);
-    res.json({
-        error: {
-            message: error.message
-        }
-    });
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  });
 });
 
 module.exports = app;
