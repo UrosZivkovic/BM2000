a6af532c43843c9e3e198d75ebfc386877683ec2
Nemanja <nemanja.m.303@gmail.com>
2018-06-17 07:01:49 +0200

    nesto nebitno

--- BM/AngularApp/ngApp/src/app/app.component.ts
+++ BM/AngularApp/ngApp/src/app/app.component.ts
@@ -1,16 +1,15 @@
-import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { AuthService } from './auth.service';
-
+import { Component } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { AuthService } from "./auth.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  readonly ROOT_URL="http://localhost:3030"
+  readonly ROOT_URL = "http://localhost:3030";
 
-  title = 'app';
-  constructor(private http: HttpClient, private _authService: AuthService){}
+  title = "app";
+  constructor(private http: HttpClient, private _authService: AuthService) {}
 }
