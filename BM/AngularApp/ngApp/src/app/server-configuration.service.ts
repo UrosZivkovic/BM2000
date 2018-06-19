ed12713680824f7ea8190519dd853ea1df5bbfbc
Nemanja <nemanja.m.303@gmail.com>
2018-06-19 16:32:20 +0200

    promenio ip

--- BM/AngularApp/ngApp/src/app/server-configuration.service.ts
+++ BM/AngularApp/ngApp/src/app/server-configuration.service.ts
@@ -5,7 +5,7 @@ import {Injectable} from '@angular/core';
 })
 export class ServerConfigurationService {
 
-  private ipAddress = '192.168.0.172';
+  private ipAddress = '192.168.0.100';
   // 192.168.0.172
   private portNum = '3030';
 
