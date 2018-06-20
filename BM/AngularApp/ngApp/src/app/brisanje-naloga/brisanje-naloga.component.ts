66e7f152af6c1fe12cd47cca263047407404822c
Urosh <uros.zivkovic96@gmail.com>
2018-06-20 19:34:52 +0200

    brisanje

---
+++ BM/AngularApp/ngApp/src/app/brisanje-naloga/brisanje-naloga.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-brisanje-naloga',
+  templateUrl: './brisanje-naloga.component.html',
+  styleUrls: ['./brisanje-naloga.component.css']
+})
+export class BrisanjeNalogaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
